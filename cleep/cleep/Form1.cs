292359d046aa6579bf60064b8e6013223f0f075c
Gavlan <alexpershin58@mail.ru>
2017-08-29 17:19:15 +0300

    Created

---
+++ cleep/cleep/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace cleep
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar=='q')
+            {
+                richTextBox1.AppendText(Clipboard.GetData(DataFormats.Text).ToString());
+            }
+        }
+    }
+}
